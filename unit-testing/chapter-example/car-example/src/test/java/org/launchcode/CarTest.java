4862e6895a339fde84addfcb95e50de934fbefc5
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-04-14 17:35:51 -0500

    complete

--- unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
@@ -1,10 +1,30 @@
 package org.launchcode;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CarTest {
+    Car test_car; // Declare the Car object here
+
+    @BeforeEach
+    public void createCarObject() {
+        // Initialize the Car object before each test
+        test_car = new Car("Toyota", "Prius", 10, 50);
+    }
+
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
+    @Test
+    public void emptyTest() {
+        assertEquals(10, 10, .001);
+    }
+
     //TODO: constructor sets gasTankLevel properly
+    @Test
+    public void testInitialGasTank() {
+        assertEquals(10, test_car.getGasTankLevel(), .001);
+    }
+
     //TODO: gasTankLevel is accurate after driving within tank range
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
     //TODO: can't have more gas than tank size, expect an exception

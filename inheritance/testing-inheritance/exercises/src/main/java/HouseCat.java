968ebff59cc41d8d6378e23592745a9dadc9e94a
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-04-18 18:18:07 -0500

    corrected

--- inheritance/testing-inheritance/exercises/src/main/java/HouseCat.java
+++ inheritance/testing-inheritance/exercises/src/main/java/HouseCat.java
@@ -2,18 +2,26 @@ public class HouseCat extends Cat {
     private String name;
     private String species = "Felis catus";
 
-    public HouseCat(String aName, double aWeight) {
-        super(aWeight);
-        name = aName;
+    public HouseCat(String name, double weight) {
+        super(weight);
+        this.name = name;
     }
 
-    public boolean isSatisfied() {
-        return !isHungry() && !isTired();
+    public static void main(String[] args) {
+        HouseCat houseCat = new HouseCat("Mina", 22);
+        System.out.println(houseCat.getFamily());
+        System.out.println(houseCat.noise());
+        Cat cat = houseCat;
+        System.out.println(cat.noise());
     }
 
     @Override
     public String noise() {
         return "Hello, my name is " + name + "!";
+    }
+
+    public boolean isSatisfied() {
+        return !isHungry() && !isTired();
     }
 
     public String purr() {
